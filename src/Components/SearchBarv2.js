62f8811224aba65f1b62d9eaff269a6913f0b662
Dylan Guidry <dylanguidry20@gmail.com>
2021-12-15 20:37:45 -0600

    results page is working now

--- src/Components/SearchBarv2.js
+++ src/Components/SearchBarv2.js
@@ -16,26 +16,39 @@ const SearchBarv2 = () => {
             .then(res => res.json())
             .then(searchData => {
                 setSearchResults(searchData.data)
-                console.log(searchData.data)
+                // console.log(searchData.data)
             })
     }
 
     return (
         <>
-            <div class="search-box fadeInUp animate one">
-                <form onSubmit={handleSubmit}>
-                    <input class='Search-Input fadeInUp animate one' onChange={(e) => handleChange(e)} value={searchInput} type="text" placeholder='Search by State Abbreviation...'></input>
-                    <i id="icon" class="search"></i>
-                </form>
-            </div>
+        <section>
+            <section class='Container'>
+                <div class="search-box fadeInUp animate one">
+                    <form class='Input-Container' onSubmit={handleSubmit}>
+                        <input class='Search-Input fadeInUp animate one' onChange={(e) => handleChange(e)} value={searchInput} type="text" placeholder='Search by State Code...'></input>
+                        <i id="icon" class="search"></i>
+                    </form>
+                </div>
+            </section>
+            <section className='all-images'>
             {searchResults.map(result => {
                 console.log(result)
+                const image = result.images[0].url;
+                // console.log(result.images[0].url)
                 return (
+                <section>
                     <div className='Result-Container'>
-                        <div className='Results'>{result.name}</div>
+                        <section class='Image-Container fadeInUp animate one'>
+                            <div className='Results'>{result.name}</div>
+                            <img class='Images-Search' src={image} alt='park'></img>
+                        </section>
                     </div>
+                </section>
                 )
             })}
+            </section>
+        </section>
         </>
     )
 }
